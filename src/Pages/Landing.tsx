766509df66d6f5cc551258797447563c7f2e8c05
Leandro Henriques <lehenriques@gmail.com>
2020-10-27 21:04:32 -0300

    formatando parte de produto

--- src/Pages/Landing.tsx
+++ src/Pages/Landing.tsx
@@ -1,35 +1,69 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
 import HeaderMenu from "../Components/HeaderMenu";
-import {Container, Row, Col, Card} from "react-bootstrap";
+import {Container, Row, Col, Card, CardColumns, Badge} from "react-bootstrap";
+import api from "../Services/api";
 
 // import '../styles/pages/landing.css';
 
 // import logoImg from '../images/logo.svg';
 
+interface Product {
+    id: number;
+    name: string;
+    percentDiscount: number;
+    defaultDisplayedPriceFormatted: number;
+    priceOld: number;
+    thumbnailUrl: string;
+    url: string;
+}
+
 function Landing() {
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        api.get('https://feirinha-back.web.app/ecwid/listar/28820124?sortBy=DEFINED_BY_STORE_OWNER&category=51824438&limit=50').then(response => {
+            setProducts(response.data);
+        });
+    }, []);
     return (
         <div id="page-landing">
-            <HeaderMenu />
+            <HeaderMenu/>
 
             <Container fluid className="menu-top">
-                <Row >
-                    <Col xs={3}>1 of 3</Col>
-                    <Col xs={9}>
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="holder.js/100px180" />
-                            <Card.Body>
-                                <Card.Title className="text-center">Tomate</Card.Title>
-                                <Card.Text>
-                                    R$ 3.10 <small>Kg</small>
-                                </Card.Text>
-                                <div className="text-center">
-                                    <Link to="/" className="btn btn-outline-link">
-                                        Adicionar
-                                    </Link>
-                                </div>
-                            </Card.Body>
-                        </Card>
+                <Row>
+                    <Col xs={2}>1 of 3</Col>
+                    <Col xs={10}>
+                        <CardColumns>
+
+                            {products.map(product => {
+                                return (
+                                    <Card style={{width: '18rem'}} key={product.id}>
+                                        <Card.Img variant="top" src={product.thumbnailUrl}/>
+                                        <Card.Body>
+                                            <Card.Title className="product_title text-center">{product.name}</Card.Title>
+                                            <Card.Text className={'product'}>
+                                                {product.priceOld ?
+                                                    <div className={'price_old'}>
+                                                        {product.priceOld} <small className={'mr-3'}>Kg</small>
+                                                    </div>
+                                                    :
+                                                    ''
+                                                }
+                                                <div className={'price'}>
+                                                {product.defaultDisplayedPriceFormatted} <small>Kg</small>
+                                                </div>
+                                            </Card.Text>
+                                            <div className="text-center">
+                                                <Link to="/" className="btn btn-outline-primary">
+                                                    Adicionar
+                                                </Link>
+                                            </div>
+                                        </Card.Body>
+                                    </Card>
+                                )
+                            })}
+                        </CardColumns>
                     </Col>
                 </Row>
             </Container>
