8b24a7e052c520ab6e357073c58600527af1bd94
Leandro Henriques <lehenriques@gmail.com>
2020-10-27 21:12:36 -0300

    Finalizando produtos e colocando rota

---
+++ src/Pages/Products.tsx
@@ -0,0 +1,74 @@
+import React, {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
+import HeaderMenu from "../Components/HeaderMenu";
+import {Container, Row, Col, Card, CardColumns, Badge} from "react-bootstrap";
+import api from "../Services/api";
+
+// import '../styles/pages/landing.css';
+
+// import logoImg from '../images/logo.svg';
+
+interface Product {
+    id: number;
+    name: string;
+    percentDiscount: number;
+    defaultDisplayedPriceFormatted: number;
+    priceOld: number;
+    thumbnailUrl: string;
+    url: string;
+}
+
+function Landing() {
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        api.get('https://feirinha-back.web.app/ecwid/listar/28820124?sortBy=DEFINED_BY_STORE_OWNER&category=51824438&limit=50').then(response => {
+            setProducts(response.data);
+        });
+    }, []);
+    return (
+        <div id="page-landing">
+            <HeaderMenu/>
+
+            <Container fluid className="menu-top pt-3">
+                <Row>
+                    <Col xs={2}>1 of 3</Col>
+                    <Col xs={10}>
+                        <CardColumns>
+
+                            {products.map(product => {
+                                return (
+                                    <Card style={{width: '18rem'}} key={product.id}>
+                                        <Card.Img variant="top" src={product.thumbnailUrl}/>
+                                        <Card.Body>
+                                            <Card.Title className="product_title text-center">{product.name}</Card.Title>
+                                            <Card.Text className={'product'}>
+                                                {product.priceOld ?
+                                                    <div className={'price_old'}>
+                                                        {product.priceOld} <small className={'mr-3'}>Kg</small>
+                                                    </div>
+                                                    :
+                                                    ''
+                                                }
+                                                <div className={'price'}>
+                                                {product.defaultDisplayedPriceFormatted} <small>Kg</small>
+                                                </div>
+                                            </Card.Text>
+                                            <div className="text-center">
+                                                <Link to="/" className="btn btn-outline-primary">
+                                                    Adicionar
+                                                </Link>
+                                            </div>
+                                        </Card.Body>
+                                    </Card>
+                                )
+                            })}
+                        </CardColumns>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+}
+
+export default Landing;
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from './Pages/Landing';
+import Products from './Pages/Products';
 
 function Routes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Landing} />
+                <Route path="/produtos" exact component={Products} />
             </Switch>
         </BrowserRouter>
     )
