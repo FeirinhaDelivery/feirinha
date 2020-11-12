593fb0abdc208078c72e7b920b41c100a59a9801
Leandro Henriques <lehenriques@gmail.com>
2020-11-11 21:58:33 -0300

    Arrumando css do sidebar

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -4,6 +4,7 @@ import {Link} from 'react-router-dom';
 
 import '../Styles/components/sidebar.css';
 import api from "../Services/api";
+import icoOffsale from "../Images/discount.svg";
 
 interface Category {
     id: string;
@@ -27,9 +28,10 @@ function Sidebar(prop: { active: string | boolean, horizontal: boolean }) {
     }, []);
     return (
         <ListGroup variant="flush" className={ 'sidebar'} horizontal={prop.horizontal}>
+            {(prop.horizontal === true) ? <Link to={"/produtos/Ofertas/10/0/0"}><ListGroup.Item className={"Ofertas"}><img src={icoOffsale} alt=""/>Ofertas</ListGroup.Item></Link> : ''}
             {categories.map(category => {
                 return (
-                    <Link to={"/produtos"+category.url.split("https://feirinha.delivery/#!")[1]+'/8/0/0'} key={category.id}>
+                    <Link to={"/produtos"+category.url.split("https://feirinha.delivery/#!")[1]+'/10/0/0'} key={category.id}>
                         <ListGroup.Item className={(prop.horizontal === true ) ? category.name : '' + activeControl(category.id) }>
                             <img src={category.imageUrl} alt=""/>
                             {category.name}
